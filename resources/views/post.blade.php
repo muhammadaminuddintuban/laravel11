e454817b1eb70842a223a4e5f7718b455bb9e55f
muhammadaminuddinwebprograming <muhammadaminuddintuban@gmail.com>
2024-06-19 22:10:23 +0700

    Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
     <article class="py-8 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 1 Januari 20124
+            <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | 1 Januari 20124
         </div>
         <p class="py-4 font-light">{{ $post['body'] }}</p>
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back To Post</a>
