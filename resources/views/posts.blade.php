0f24cf02b4234dd6e13d8edc7885cb2eaf6f641a
muhammadaminuddinwebprograming <muhammadaminuddintuban@gmail.com>
2024-06-21 22:39:03 +0700

    Menambahkan Post Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,10 +4,14 @@
     @foreach ($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
         <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         </a>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans() }}
+        <div>
+            By
+            <a href="/authors/{{ $post->author->username }}" class="hover:underline text-base text-gray-500">{{ $post->author->name }}</a> 
+            in 
+            <a href="/categories/{{ $post->category->slug }}" class="hover:underline">{{ $post->category->name }}</a> |
+            {{ $post->created_at->diffForHumans() }}
         </div>
         <p class="py-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
         <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Readmore &raquo;</a>
